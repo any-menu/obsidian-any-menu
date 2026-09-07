d0c6e97b20ca2d68395d3ed5a1119837b2a4f2da
LincZero <762699299@qq.com>
2026-09-07 16:16:44 +0000

    chore: sync from monorepo

--- store/dict/md_color_highlight.js
+++ store/dict/md_color_highlight.js
@@ -4,13 +4,14 @@ let cache_hoverEl = null // 悬浮显示的自定义面板
 let cache_el_am_icon = null // 工具栏按钮的图标
 
 const emoji_dict = {
+    'null':     '', // 特殊: 无着色高量，即普通的默认的 Markdown 高亮
     'red':      '🟥',
     'blue':     '🟦',
     'green':    '🟩',
-    'brown':    '🟫',
-    'orangered':'🟧',
     'yellow':   '🟨',
     'purple':   '🟪',
+    'orangered':'🟧',
+    'brown':    '🟫',
     'black':    '⬛',
     'white':    '⬜'
 }
@@ -41,31 +42,29 @@ export default {
         }
         const cache_emoji = emoji_dict[cache_color]
 
-        // b1. 选中的文本最外层是 span，则修改属性 (可能之前设置过文字色或背景色，不要再套一层，会较臃肿)
+        // b1. `==xx==` 形式
         const spanMatch = str.match(/^==(.)(.*)==$/u); // 注意 `u` 模式保证第一个匹配项是完整的 Unicode/Emoji 字符
         if (spanMatch) {
             // 解析标签
             let emoji = spanMatch[1];
             let newStr = spanMatch[2];
-
-            // b11. 已有 highlight-emoji 属性
-            if (cache_emoji) {
-                // 颜色相同 → 移除声明
-                if (emoji == cache_emoji) {
-                    this.app.api.sendText(newStr); return;
-                }
-                // 颜色不同 → 替换为新颜色
-                else {
-                    this.app.api.sendText(`==${cache_emoji}${newStr}==`); return;
-                }
+            if (!Object.values(emoji_dict).includes(emoji)) {
+                newStr = emoji + newStr
+                emoji = ''
             }
-            // b12. 没有 highlight-emoji 属性，追加
+
+            // `==🟥xx==` --🟥--> `xx`。颜色相同则移除声明
+            if (emoji == cache_emoji) {
+                this.app.api.sendText(newStr); return;
+            }
+            // `==🟥xx==` --🟦--> `==🟦xx==`。颜色不同则替换为新颜色/空颜色
             else {
-                this.app.api.sendText(`==${cache_emoji}${emoji}${newStr}==`); return;
+                this.app.api.sendText(`==${cache_emoji}${newStr}==`); return;
             }
         }
-        // b2. 为选中文本包裹 span 标签
+        // b2. 无 `==` 包裹形式
         else {
+            // `xx` --🟥--> `==🟥xx==`
             this.app.api.sendText(`==${cache_emoji}${str}==`); return;
         }
     },
@@ -112,7 +111,8 @@ export default {
         const root = document.createElement('div')
             root.className = 'md-color-highlight-panel'
         
-        for (const [key, value] of Object.entries(emoji_dict)) {
+        for (let [key, value] of Object.entries(emoji_dict)) {
+            if (key == 'null') value = '🟨' // 特殊，空emoji模式用首个黄色来表示
             const item = document.createElement('span');
                 root.appendChild(item);
                 item.innerText = value;
